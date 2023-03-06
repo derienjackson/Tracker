378c2d371fd9e1878e8dfa695040917898361f2a
Derien <112275650+derienjackson@users.noreply.github.com>
2023-03-06 09:36:45 -0800

    Add files via upload

---
+++ trackserver/src/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function comparePassword(
+  candidatePassword
+) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      if (isMatch) {
+        return resolve(true);
+      } else {
+        return reject(false);
+      }
+    });
+  });
+};
+
+mongoose.model('User', userSchema);
